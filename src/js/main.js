54157f6d9e2fbcf5d86699fb0a148c11aae99510
Matheus Schalch <matheus.schalch@gmail.com>
2026-08-27 21:47:34 -0300

    feat: navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 import {mapaderotas} from "./rotas/rotas.js"
+import {navbar} from "./navbar/navbar.js"
+
+navbar(mapaderotas)
 
 console.log(mapaderotas)
+console.log(mapaderotas[0])
+console.log(mapaderotas[0].label)
