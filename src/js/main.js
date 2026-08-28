c198efc350c5005f07c8b7ccf6d58b8fee5d034a
Matheus Schalch <matheus.schalch@gmail.com>
2026-08-27 22:00:47 -0300

    feat: roteador no main

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,18 @@
 import {mapaderotas} from "./rotas/rotas.js"
 import {navbar} from "./navbar/navbar.js"
 
-navbar(mapaderotas)
+const app = document.getElementById("app")
 
-console.log(mapaderotas)
-console.log(mapaderotas[0])
-console.log(mapaderotas[0].label)
+navbar(mapaderotas)
+// #home
+let hash = location.hash || "#home"
+
+//console.log(mapaderotas)
+//console.log(mapaderotas[0])
+//console.log(mapaderotas[0].label)
+
+const roteador = {}
+
+window.addEventListener("hashchange",()=>{
+    mapaderotas[1].pagina(app)
+})
